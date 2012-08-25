3919f4cb3b6f65301cefe2afb6ce555b54ffaba6
Christian Götz <Christian.G2003@gmail.com>
2012-08-25 09:57:46 +0200

    Just a basic starting point
    No functionality, just a basic skeleton to build upon.

---
+++ Source/Talisa.Contracts/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SachsenCoder.Talisa.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/Talisa.Core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SachsenCoder.Talisa.Core
+{
+    public class Class1
+    {
+    }
+}
